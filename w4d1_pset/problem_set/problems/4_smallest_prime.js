f61f279e87cc1d2733c40cf8e7fd179766928401
zeisen33 <110428373+zeisen33@users.noreply.github.com>
2022-08-23 06:42:47 -0400

    w4d1 pset done

---
+++ w4d1_pset/problem_set/problems/4_smallest_prime.js
@@ -0,0 +1,55 @@
+/***********************************************************************
+Write a function `smallestPrime(array)` that takes in an array of
+numbers and returns the smallest prime number in the array.
+
+HINTS:
+- start by creating an `isPrime` helper function
+- the smallest prime number is 2
+
+Examples:
+
+var arr1 = [ 6, 7, 12, 11, 5, 4 ];
+smallestPrime(arr1); // => 5
+
+var arr2 = [11, -7, 7, 8, 6, 10];
+smallestPrime(arr2); // => 7
+
+var arr3 = [];
+smallestPrime(arr3); // => null
+
+var arr4 = [4, 6, 8, 10];
+smallestPrime(arr4); // => null
+***********************************************************************/
+function isPrime(n) {
+    if (n < 2) {
+        return false
+    } else if (n === 2) {
+        return true
+    } else {
+        for (let i = 2; i < n; i++) {
+            if (n % i === 0) {
+                return false
+            }
+        }
+    }
+    return true
+}
+
+function smallestPrime(array) {
+    let primes = []
+    let minSoFar = null
+    for (let i = 0; i < array.length; i++) {
+        if (isPrime(array[i])) {
+            primes.push(array[i])
+        }
+    }
+    for (let j = 0; j < primes.length - 1; j++) {
+        minSoFar = Math.min(primes[j], primes[j + 1])       // how would i do this with .Map
+    }
+    return minSoFar
+}
+var arr2 = [11, -7, 7, 8, 6, 10];
+console.log(smallestPrime(arr2)); // => 7
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+module.exports = smallestPrime;
